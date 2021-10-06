224656e32a21f6963057803e6114de15d2234fd7
Alex Welcing <alexwelcing@gmail.com>
2021-10-06 14:06:05 -0400

    swap cats

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,12 @@
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
       <h1 class="title text-center">20 — 22 projects — 22</h1>
-      <h2 class="subtitle text-center">Knowledge work in public.</h2>
+      <h2 class="subtitle text-center">Interactive project planning.</h2>
     </section>
 
     <section class="mt-8">
       <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest projects</h3>
-      <posts post-type="blog" :amount="1" />
+      <posts post-type="projects" :amount="3" />
     </section>
   </main>
 </template>
