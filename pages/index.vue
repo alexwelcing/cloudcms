6220e2feaa1d3adeb3d4f59a903c45091953698f
Alex Welcing <alexwelcing@gmail.com>
2021-11-10 13:20:29 -0500

    only real

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <main>
-    <section class="self-center flex flex-col flex-1 items-center justify-center">
+    <section class="mt-8 self-center flex flex-col flex-1 items-center justify-center">
       <h1 class="title text-center">20 — 22 projects — 22</h1>
       <h2 class="subtitle text-center">Interactive project planning.</h2>
     </section>
 
     <section class="mt-8">
       <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest projects</h3>
-      <posts post-type="projects" :amount="2" />
+      <posts post-type="projects" :amount="3" />
     </section>
   </main>
 </template>
